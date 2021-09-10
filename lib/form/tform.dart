2173498c63fe4d47d74f7fb7d104be68591a0974
程序鹿 <18866619695@163.com>
2021-09-10 09:06:52 +0800

    删除无用代码

--- lib/form/tform.dart
+++
@@ -1,5 +0,0 @@
-export 'form.dart';
-export 'form_row.dart';
-export 'form_cell.dart';
-export 'form_validation.dart';
-export 'form_selector_page.dart';
--- lib/widgets/photos_cell.dart
+++ lib/widgets/photos_cell.dart
@@ -3,52 +3,6 @@ import 'package:good_grandma/provider/image_provider.dart';
 import 'package:good_grandma/widgets/select_image.dart';
 import 'package:provider/provider.dart';
 
-import '../form/tform.dart';
-
-///自定义图片选择器
-class CustomPhotosWidget extends StatelessWidget {
-  CustomPhotosWidget({
-    Key key,
-    this.row,
-  }) : super(key: key);
-
-  final TFormRow row;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-      child: Column(
-        children: [
-          Container(
-            height: 48,
-            alignment: Alignment.centerLeft,
-            child: Text(
-              row.title,
-              style: TextStyle(fontSize: 15, color: Colors.black87),
-            ),
-          ),
-          Container(
-            height: 100,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: (row.state as List).length,
-              itemBuilder: (BuildContext context, int index) {
-                return SelectImageView(
-                  selected: (image) async {
-                    //实际情况是上传照片 返回图片URL，这里模拟数据使用路径
-                    row.state[index]["picurl"] = image.path;
-                  },
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 ///自定义多图片选择器
 class CustomPhotoWidget extends StatelessWidget {
   CustomPhotoWidget({Key key,
