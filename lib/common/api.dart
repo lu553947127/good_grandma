0dd7d3289ca91240cf19d48993b4231c19322f1b
xujie <sdutxujie@163.com>
2021-09-27 13:49:18 +0800

    api格式化

--- lib/common/api.dart
+++ lib/common/api.dart
@@ -1,7 +1,5 @@
-
 ///服务service
 class Api {
-
   static String baseUrl() {
     /// 外网
     // return 'http://47.100.191.162';
@@ -32,7 +30,8 @@ class Api {
   static const startProcess = "/api/hap-workflow/process/startProcess";
 
   ///获取流程表单
-  static const getFormByProcessId = "/api/hap-workflow/process/getFormByProcessId";
+  static const getFormByProcessId =
+      "/api/hap-workflow/process/getFormByProcessId";
 
   ///获取流程详情
   static const processDetail = "/api/hap-workflow/process/detail";
@@ -47,13 +46,13 @@ class Api {
   static const visitPlanList = "/api/hap-app1/app/visitplan/list";
 
   ///拜访计划新增
-  static const visitPlanAdd= "/api/hap-app1/app/visitplan/submit";
+  static const visitPlanAdd = "/api/hap-app1/app/visitplan/submit";
 
   ///市场物料列表
   static const materialList = "/api/hap-app1/app/material/list";
 
   ///市场物料添加
-  static const materialAdd= "/api/hap-app1/app/material/submit";
+  static const materialAdd = "/api/hap-app1/app/material/submit";
 
   ///市场物料详情
   static const materialDetail = "/api/hap-app1/app/material/detail";
@@ -68,16 +67,18 @@ class Api {
   static const getDeptName = "/api/hap-app1/app/system/getDeptName";
 
   ///客户拜访列表
-  static const customerVisitList= "/api/hap-app1/app/visitplan/customerVisitList";
+  static const customerVisitList =
+      "/api/hap-app1/app/visitplan/customerVisitList";
 
   ///客户拜访新增
-  static const customerVisitAdd= "/api/hap-app1/app/visitplan/customerVisitSave";
+  static const customerVisitAdd =
+      "/api/hap-app1/app/visitplan/customerVisitSave";
 
   ///客户列表
-  static const customerList= "/api/hap-app1/app/user/areaUser";
+  static const customerList = "/api/hap-app1/app/user/areaUser";
 
   ///员工列表
-  static const userList= "/api/hap-app1/app/user/areaSaleUsers";
+  static const userList = "/api/hap-app1/app/user/areaSaleUsers";
 
   ///冰柜销量列表
   static const freezerSalesList = "/api/hap-app3/app/freezer/freezerSales";
@@ -90,64 +91,103 @@ class Api {
 
   ///文件柜列表
   static const fileCabinetList = "/api/hap-app1/app/file/selectFileFolder";
+
   ///报修列表
-  static const getFreezerRepairList = "/api/hap-app3/app/freezerRepair/getFreezerRepairList";
+  static const getFreezerRepairList =
+      "/api/hap-app3/app/freezerRepair/getFreezerRepairList";
+
   ///签到
   static const signAdd = "/api/hap-apptony/app/sign/signAdd";
+
+  ///签到统计
   static const signList = "/api/hap-apptony/app/sign/signList";
+
   ///规章文件
   static const regularDocList = "/api/hap-app3/app/manage/list";
+
   ///消息
   static const getCategoryCount = "/api/hap-app3/app/notice/getCategoryCount";
+
   ///公告list
-  static const getNoticeCategoryList = "/api/hap-app3/app/notice/getNoticeCategoryList";
+  static const getNoticeCategoryList =
+      "/api/hap-app3/app/notice/getNoticeCategoryList";
+
   ///消息已读
   static const settingRead = "/api/hap-app3/app/notice/settingRead";
+
   ///客户库存list
   static const customerStockList = "/api/hap-app3/app/customerStock/list";
+
   ///库存商品list
-  static const customerStockGoodsList = "/api/hap-app3/app/goods/goodsListStock";
+  static const customerStockGoodsList =
+      "/api/hap-app3/app/goods/goodsListStock";
+
   ///商品list
   static const goodsList = "/api/hap-app3/app/goods/goodsList";
+
   ///新增库存
   static const customerStockAdd = "/api/hap-app3/app/customerStock/addStock";
+
   ///库存detail
   static const customerStockDetail = "/api/hap-app3/app/customerStock/detail";
+
   ///获取用户信息
   static const getUserInfoById = "/api/hap-app/app/user/loginUser";
+
   ///报告列表
   static const reportList = "/api/hap-apptony/report/reportList";
+
   ///提交日报
   static const reportDayAdd = "/api/hap-apptony/app/report/reportDayAdd";
+
   ///提交week报
   static const reportWeekAdd = "/api/hap-apptony/app/report/reportWeekAdd";
+
   ///提交Month报
   static const reportMonthAdd = "/api/hap-apptony/app/report/reportMonthAdd";
+
   ///区域选择
   static const regionList = "/api/hap-apptony/app/report/regionList";
+
   ///筛选下级员工
   static const reportUserList = "/api/hap-apptony/app/report/userList";
+
   ///客户反馈
   static const feedback = "/api/hap-app3/app/feedback/save";
+
   ///轨迹查询
-  static const trajectoryList = "/api/hap-apptony/app/trajectory/trajectoryList";
+  static const trajectoryList =
+      "/api/hap-apptony/app/trajectory/trajectoryList";
+
   ///下单
   static const orderAdd = "/api/hap-apptony/app/order/orderAdd";
+
   ///订单列表
   static const orderList = "/api/hap-apptony/app/order/orderList";
+
   ///订单detail
   static const orderDetail = "/api/hap-apptony/app/order/orderDetail";
+
   ///订单Confirm
   static const orderConfirm = "/api/hap-apptony/app/order/orderConfirm";
-  ///feedbackType
-  static const feedbackType = "/api/hap-system3/dict/dictionary?code=feedback_type";
-  ///业绩统计
-  static const selectContractStatistics = "/api/hap-app1/app/contract/selectContractStatistics";
-  ///查询下级所有业绩总和
-  static const selectSaleSubordinate = "/api/hap-app1/app/contract/selectSaleSubordinate";
-  ///业务人员统计每个月业绩
-  static const selectSaleMonthStatistics = "/api/hap-app1/app/contract/selectSaleMonthStatistics";
-  ///客户统计每个月业绩
-  static const selectMonthStatistics = "/api/hap-app1/app/contract/selectMonthStatistics";
 
+  ///feedbackType
+  static const feedbackType =
+      "/api/hap-system3/dict/dictionary?code=feedback_type";
+
+  ///业绩统计
+  static const selectContractStatistics =
+      "/api/hap-app1/app/contract/selectContractStatistics";
+
+  ///查询下级所有业绩总和
+  static const selectSaleSubordinate =
+      "/api/hap-app1/app/contract/selectSaleSubordinate";
+
+  ///业务人员统计每个月业绩
+  static const selectSaleMonthStatistics =
+      "/api/hap-app1/app/contract/selectSaleMonthStatistics";
+
+  ///客户统计每个月业绩
+  static const selectMonthStatistics =
+      "/api/hap-app1/app/contract/selectMonthStatistics";
 }
