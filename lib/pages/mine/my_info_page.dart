319c16e019a804fc670093b446075bb900ab53bd
xujie <sdutxujie@163.com>
2021-09-27 14:18:21 +0800

    我的信息页面显示真实数据

--- lib/pages/mine/my_info_page.dart
+++ lib/pages/mine/my_info_page.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:good_grandma/common/api.dart';
+import 'package:good_grandma/common/http.dart';
 import 'package:good_grandma/common/loading_widget.dart';
+import 'package:good_grandma/common/log.dart';
 import 'package:good_grandma/common/my_cache_image_view.dart';
+import 'package:good_grandma/common/store.dart';
 import 'package:good_grandma/common/utils.dart';
 import 'package:good_grandma/pages/mine/alert_name_page.dart';
 import 'package:good_grandma/pages/mine/alert_phone_page.dart';
@@ -24,176 +29,139 @@ class _MyInfoPageState extends State<MyInfoPage> {
   String _gender = '';
   String _phone = '';
   String _area = '';
-  String _province = '';
-  String _city = '';
-
-  ///0:loading 1:success 2:fail
-  int _state = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _refresh();
-  }
 
   @override
   Widget build(BuildContext context) {
     final divider = const Divider(height: 1, indent: 10.0, endIndent: 10.0);
     return Scaffold(
       appBar: AppBar(title: const Text('我的信息')),
-      body: _state == 0
-          ? LoadingWidget()
-          : _state == 2
-              ? LoadFailWidget(
-                  retryAction: () {
-                    setState(() => _state = 0);
-                    _refresh();
-                  },
-                )
-              : Scrollbar(
-                  child: CustomScrollView(
-                    slivers: [
-                      SliverPadding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 15.0, vertical: 10.0),
-                        sliver: SliverToBoxAdapter(
-                          child: Card(
-                            child: Column(
-                              children: [
-                                AvatarCell(avatar: _avatar),
-                                divider,
-                                ListTile(
-                                  title: Row(
-                                    children: [
-                                      const Text('姓名'),
-                                      Expanded(
-                                          child: Text(
-                                        _name,
-                                        textAlign: TextAlign.end,
-                                      )),
-                                    ],
-                                  ),
-                                  trailing: Icon(Icons.chevron_right),
-                                  onTap: () async {
-                                    String name = await Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (_) =>
-                                                AlertNamePage(name: _name)));
-                                    if (name != null &&
-                                        name.isNotEmpty &&
-                                        name.length == 11 &&
-                                        name != _phone) {
-                                      setState(() => _name = name);
-                                    }
-                                  },
+      body: RefreshIndicator(
+        onRefresh: () async => setState(() {}),
+        child: FutureBuilder(
+          future: requestGet(Api.getUserInfoById,
+              param: {'userId': Store.readUserId()}),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.hasData) {
+              LogUtil.d('getUserInfoById value = ${snapshot.data}');
+              Map result = jsonDecode(snapshot.data.toString());
+              Map data = result['data'];
+              _avatar = data['avatar'] ?? '';
+              _name = data['name'] ?? '';
+              int sex = data['sex'] ?? 1;
+              if (sex == 1)
+                _gender = '男';
+              else
+                _gender = '女';
+              _phone = data['phone'] ?? '';
+              _area = data['deptName'] ?? '';
+              return Scrollbar(
+                child: CustomScrollView(
+                  slivers: [
+                    SliverPadding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 15.0, vertical: 10.0),
+                      sliver: SliverToBoxAdapter(
+                        child: Card(
+                          child: Column(
+                            children: [
+                              AvatarCell(avatar: _avatar),
+                              divider,
+                              ListTile(
+                                title: Row(
+                                  children: [
+                                    const Text('姓名'),
+                                    Expanded(
+                                        child: Text(_name,
+                                            textAlign: TextAlign.end)),
+                                  ],
                                 ),
-                                divider,
-                                ListTile(
-                                  title: Row(
-                                    children: [
-                                      const Text('性别'),
-                                      Spacer(),
-                                      Text(_gender),
-                                    ],
-                                  ),
-                                  trailing: Icon(Icons.chevron_right),
-                                  onTap: () async {
-                                    String result =
-                                        await showPicker(['男', '女'], context);
-                                    if (result != null)
-                                      setState(() => _gender = result);
-                                  },
+                                trailing: Icon(Icons.chevron_right),
+                                onTap: () async {
+                                  String name = await Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (_) =>
+                                              AlertNamePage(name: _name)));
+                                  if (name != null &&
+                                      name.isNotEmpty &&
+                                      name.length == 11 &&
+                                      name != _phone) {
+                                    setState(() => _name = name);
+                                  }
+                                },
+                              ),
+                              divider,
+                              ListTile(
+                                title: Row(
+                                  children: [
+                                    const Text('性别'),
+                                    Spacer(),
+                                    Text(_gender)
+                                  ],
                                 ),
-                                divider,
-                                ListTile(
-                                  title: Row(
-                                    children: [
-                                      const Text('手机号'),
-                                      Spacer(),
-                                      Text(_phone),
-                                    ],
-                                  ),
-                                  trailing: Icon(Icons.chevron_right),
-                                  onTap: () async {
-                                    String phone = await Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (_) => AlertPhonePage()));
-                                    if (phone != null &&
-                                        phone.isNotEmpty &&
-                                        phone.length == 11 &&
-                                        phone != _phone) {
-                                      setState(() => _phone = phone);
-                                    }
-                                  },
+                                trailing: Icon(Icons.chevron_right),
+                                onTap: () async {
+                                  String result =
+                                      await showPicker(['男', '女'], context);
+                                  if (result != null && result.isNotEmpty)
+                                    setState(() => _gender = result);
+                                },
+                              ),
+                              divider,
+                              ListTile(
+                                title: Row(
+                                  children: [
+                                    const Text('手机号'),
+                                    Spacer(),
+                                    Text(_phone),
+                                  ],
                                 ),
-                              ],
-                            ),
+                                trailing: Icon(Icons.chevron_right),
+                                onTap: () async {
+                                  String phone = await Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (_) => AlertPhonePage()));
+                                  if (phone != null &&
+                                      phone.isNotEmpty &&
+                                      phone.length == 11 &&
+                                      phone != _phone) {
+                                    setState(() => _phone = phone);
+                                  }
+                                },
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                      SliverPadding(
-                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                        sliver: SliverToBoxAdapter(
-                          child: Card(
-                            child: Column(
-                              children: [
-                                ListTile(
-                                  title: const Text('大区'),
-                                  trailing: Text(_area),
-                                  onTap: () async {
-                                    String result = await showPicker(
-                                        ['大区1', '大区2'], context);
-                                    if (result != null)
-                                      setState(() => _area = result);
-                                  },
-                                ),
-                                divider,
-                                ListTile(
-                                  title: const Text('省'),
-                                  trailing: Text(_province),
-                                  onTap: () async {
-                                    String result =
-                                        await showPicker(['省1', '省2'], context);
-                                    if (result != null)
-                                      setState(() => _province = result);
-                                  },
-                                ),
-                                divider,
-                                ListTile(
-                                  title: const Text('城市'),
-                                  trailing: Text(_city),
-                                  onTap: () async {
-                                    String result = await showPicker(
-                                        ['城市1', '城市2'], context);
-                                    if (result != null)
-                                      setState(() => _city = result);
-                                  },
-                                ),
-                              ],
-                            ),
+                    ),
+                    SliverPadding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      sliver: SliverToBoxAdapter(
+                        child: Card(
+                          child: Column(
+                            children: [
+                              ListTile(
+                                title: const Text('大区'),
+                                trailing: Text(_area),
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
+              );
+            }
+            if (snapshot.hasError) {
+              return NoDataWidget(emptyRetry: () => setState(() {}));
+            }
+            return LoadingWidget();
+          },
+        ),
+      ),
     );
-  }
-
-  void _refresh() async {
-    await Future.delayed(Duration(seconds: 1));
-    _avatar =
-        'https://c-ssl.duitang.com/uploads/item/201707/28/20170728212204_zcyWe.thumb.1000_0.jpeg';
-    _name = '王光宇';
-    _gender = '男';
-    _phone = '18888666669';
-    _area = '';
-    _province = '';
-    _city = '';
-    _state = 1;
-    if (mounted) setState(() {});
   }
 
   @override
@@ -280,7 +248,7 @@ class _AvatarCellState extends State<AvatarCell> {
 
   Future<bool> _getImage(ImageSource source) async {
     try {
-      final pickedFile = await _picker.getImage(source: source);
+      final pickedFile = await _picker.pickImage(source: source);
       if (pickedFile != null) {
         _image = File(pickedFile.path);
         return true;
